875dfd44969b7f6e3e56f343420455f9edce87ed
arianaborrajo <borrajoarias@gmail.com>
2024-11-29 14:13:16 +0100

    feat: Empezar caracteristicas

--- Personajes.py
+++ Personajes.py
@@ -1,11 +1,11 @@
-Cartas = {'Susan' {Pelo = 'Blanco', Boca = 'Grande', Ojos = 'Marrones', Cara = 'Alargada', Nariz = 'Pequeña', Piel = 'Blanca'}
+Cartas = {'Susan': {Pelo = 'Blanco', Boca = 'Grande', Ojos = 'Marrones', Cara = 'Alargada', Nariz = 'Pequeña', Piel = 'Blanca'}
 , 'Claire', 'David', 'Anne', 'Robert', 'Anita', 
 'Joe', 'George', 'Bill', 'Alfred', 'Max', 'Tom', 'Alex', 'Sam', 'Richard', 'Paul', 
 'Maria', 'Frans', 'Herman', 'Bernard', 'Philip', 'Eric', 'Charles', 'Peter'}
 
 
 
-Susan = {Pelo = 'Blanco', Boca = 'Grande', Ojos = 'Marrones', Cara = 'Alargada', Nariz = 'Pequeña', Piel = 'Blanca'}
+Susan = {Genero = 'Mujer', Pelo = 'Liso', Color_pelo = 'Blanco', Ojos = 'Marrones', Nariz = 'Pequeña', Gafas = 'False', Bigote = 'False', Barba = 'False', Gorro = 'False'}
 
 Claire = {Pelo = 'Pelirrojo', Boca = 'Pequeña', Ojos = 'Marrones', Cara = 'Redonda', Nariz = 'Pequeña', Piel = 'Blanca'}
 
