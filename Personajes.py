8a964ae981e65524a5e8464d841565cff7f9deab
arianaborrajo <borrajoarias@gmail.com>
2024-11-29 13:49:51 +0100

    docs: :busts_in_silhouette: Añadir diccionario con nombres

---
+++ Personajes.py
@@ -0,0 +1,25 @@
+Cartas = {'Susan' {Pelo = 'Blanco', Boca = 'Grande', Ojos = 'Marrones', Cara = 'Alargada', Nariz = 'Pequeña', Piel = 'Blanca'}
+, 'Claire', 'David', 'Anne', 'Robert', 'Anita', 
+'Joe', 'George', 'Bill', 'Alfred', 'Max', 'Tom', 'Alex', 'Sam', 'Richard', 'Paul', 
+'Maria', 'Frans', 'Herman', 'Bernard', 'Philip', 'Eric', 'Charles', 'Peter'}
+
+
+
+Susan = {Pelo = 'Blanco', Boca = 'Grande', Ojos = 'Marrones', Cara = 'Alargada', Nariz = 'Pequeña', Piel = 'Blanca'}
+
+Claire = {Pelo = 'Pelirrojo', Boca = 'Pequeña', Ojos = 'Marrones', Cara = 'Redonda', Nariz = 'Pequeña', Piel = 'Blanca'}
+
+David = {Pelo = 'Rubio', Boca = 'Fina', Ojos = 'Marrones', Cara = 'Alargada', Nariz = 'Pequeña', Piel = 'Blanca', Barba? }
+Anne = {Pelo = 'Marron', Boca = 'Pequeña', Ojos = 'Marrones', Cara = 'Redonda' Nariz = 'Grande', Piel = 'Negra' pendientes?}
+Robert = {Pelo = 'Marron', Boca = 'Fina', Ojos = 'Azules', Cara = 'Alargada', Nariz = 'Grande', Piel = 'Blanca'}
+Anita 
+Joe 
+George 
+Bill 
+Alfred 
+Max 
+Tom 
+Alex 
+Sam 
+Richard 
+Paul
