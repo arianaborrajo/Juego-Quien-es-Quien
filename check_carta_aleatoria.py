79ecce05e9e700754165982d18d2808478fd52c4
arianaborrajo <borrajoarias@gmail.com>
2024-12-05 14:11:00 +0100

    feat: Seguir con más casos test para la carta al azar

--- check_carta_aleatoria.py
+++ check_carta_aleatoria.py
@@ -5,3 +5,6 @@ def test_una_sola_carta():
     personaje_oculto = seleccionar_personaje(personaje)
     assert seleccionar_carta == "personaje1", "No seleccionó la única carta disponible."
 
+def test_seleccionar_carta_aleatoria():
+    carta_seleccionada = seleccionar_carta(personajes)
+    assert carta_seleccionada in personajes, "La carta seleccionada no pertenece al conjunto."
